52b0596ae2b387e63a9d5b1e022ce235960adc6f
Redwan Ul Haq Choyon <redwan.choyon.bd@gmail.com>
2024-10-19 21:33:25 -0600

    Game without audio

--- godot_creep_game/player.gd
+++ godot_creep_game/player.gd
@@ -1,18 +1,14 @@
 extends Area2D
 signal hit
+
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
 	screen_size = get_viewport_rect().size
-	hide()
+	hide() # Comment if you need to see player movement
 	
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
@@ -40,9 +36,12 @@ func _process(delta: float) -> void:
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
-
-
-func _on_body_entered(body: Node2D) -> void:
+	if velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.flip_h = false
+		
+func _on_body_entered(body):
 	hide() # Player disappears after being hit.
 	hit.emit()
 	# Must be deferred as we can't change physics properties on a physics callback.
