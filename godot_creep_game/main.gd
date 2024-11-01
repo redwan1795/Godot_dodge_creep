46aa9d890be737c80ec66ecccd652974ad2b53e4
Redwan Ul Haq Choyon <redwan.choyon.bd@gmail.com>
2024-11-01 15:55:37 -0600

    Added sound in the code

--- godot_creep_game/main.gd
+++ godot_creep_game/main.gd
@@ -3,11 +3,28 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 
+#var background_music = preload("res://music/410574__yummie__game-background-music-loop-short.mp3")
+
+# Dictionary to store audio files
+#var audio_files = {}
+# Variable to keep track of the currently playing AudioStreamPlayer
+var current_audio_player: AudioStreamPlayer2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass #new_game()
-
+	
+	
+	# Load music files from the directory
+	#var dir = DirAccess.open("res://music")
+	#if dir != null:
+	#	var file_name = dir.get_next()
+	#	while file_name != "":
+	#		if !dir.current_is_dir() and file_name.ends_with(".mp3"):
+	#			var file_path = "res://music/" + file_name
+	#			audio_files[file_name] = load(file_path)
+	#		file_name = dir.get_next()
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
@@ -18,6 +35,10 @@ func game_over() -> void:
 	$MobTimer.stop() 
 	$HUD.show_game_over()
 	
+	#Stop the audio when the game is over
+	stop_audio()
+	
+	
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
@@ -25,7 +46,13 @@ func new_game():
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
 	get_tree().call_group("mobs", "queue_free")
-
+	
+	#start the audio when the game starts
+	#play_audio(background_music)
+	#play_audio("410574__yummie__game-background-music-loop-short.mp3")
+	
+	current_audio_player  = $GameAudioPlayer	
+	current_audio_player.play()
 
 func _on_mob_timer_timeout() -> void:
 	var mob = mob_scene.instantiate()
@@ -58,3 +85,27 @@ func _on_score_timer_timeout() -> void:
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
 	$ScoreTimer.start()
+	
+#func play_audio(file_name) -> void:
+	# Check if the audio file exists in the dictionary
+	#if file_name in audio_files:
+		# Stop any currently playing audio
+		#stop_audio()
+
+		# Create a new AudioStreamPlayer and set the stream
+		#current_audio_player = AudioStreamPlayer.new()
+		#current_audio_player.volume_db = 0.0 
+		#current_audio_player.stream = load(file_path)
+		#add_child(current_audio_player)
+		
+		# Start playing the audio
+		
+
+
+func stop_audio():
+	# Stop and remove the current audio player if it exists
+	if current_audio_player:
+		current_audio_player.stop()
+		#remove_child(current_audio_player)
+		#current_audio_player.queue_free()
+		#current_audio_player = null	
